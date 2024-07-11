700d713b6317952351e5e4f7d7cacbf977a8457f
alexnezami <alireza.nezami@gmail.com>
2024-07-11 12:31:18 -0400

    js practicing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-﻿console.log(2 + 6);
+﻿"use strict";
+// we use this script for undefined variable & sensetive to reserve word
+/* console.log(2 + 6);
+console.log(typeof 2);
+console.log(3 === 3);
+console.log(typeof true);
+let firstName;
+console.log(typeof firstName); => undefined 
+let firstName;
+firstName = "Alex";
+console.log(firstName);
+console.log(typeof firstName);
+firstName = 12;
+console.log(firstName);
+console.log(typeof firstName);
+// // we can change type of variable
+let age = 28;
+age += 1;
+console.log(age);
+
+lastName = "change";
+out = `this is a test
+statement 2 ${firstName} ${3 * 6}
+statement 3`;
+console.log(out);
+const gpa = 3.8;
+const IELTS = 7.5;
+const isEligible = gpa >= 3.6 && IELTS >= 7;
+console.log(isEligible);
+const isEligible2 = gpa >= 6 || IELTS >= 7;
+console.log(isEligible2);
+// for not we use ! sign
+gpa >= 3.6 && IELTS >= 7
+  ? console.log("you are eligible")
+  : console.log("you are not eligible");
+
+const tempresult =
+  gpa >= 3.6 && IELTS >= 7
+    ? "you are eligible with status"
+    : "you are not eligible with status";
+console.log(tempresult);
+
+let idxs = 10;
+idx = 5;
+console.log(idx);
+*/
