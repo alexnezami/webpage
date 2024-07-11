0cc6f12e10d7fdb8260dab596c49374e4f135de8
alexnezami <alireza.nezami@gmail.com>
2024-07-11 18:10:21 -0400

    js practicing 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 ﻿"use strict";
+document.addEventListener("keydown", (event) => {
+  if (event.key === "u" && event.ctrlKey) {
+    event.preventDefault();
+  }
+});
+
 // we use this script for undefined variable & sensetive to reserve word
 /* console.log(2 + 6);
 console.log(typeof 2);
@@ -43,4 +49,96 @@ console.log(tempresult);
 let idxs = 10;
 idx = 5;
 console.log(idx);
+
+const header = document.querySelector(".header__ul");
+console.log(header);
+
+const divs = document.querySelectorAll("div");
+console.log(divs);
+
+divs[0].style.backgroundColor = "red";
+
+for (let i = 0; i < divs.length; i++) {
+  console.log(divs[i]);
+}
+
+console.log(header.children);
+
+const mobileNav = document.querySelector(".header__mobile-nav");
+const headerNav = document.querySelector(".header__ul");
+
+mobileNav.addEventListener("click", () => {
+  headerNav.classList.toggle("active");
+});
+
+// with keypress we can use arrow buttons
+// with keyup we can use when we unleash key
+// document.addEventListener("keydown", (event) => {
+//   alert(event.ctrlKey);
+// });
+
+const company = {
+  name: "Google",
+  jobs: ["SE", "PM", "DS", "QA"],
+  location: "US",
+};
+
+const [first, , second, ...rest] = company.jobs;
+console.log(first);
+console.log(second);
+console.log(rest);
+console.log(...rest);
+const { jobs: companyJobs, name: companyName } = company;
+console.log(companyJobs);
+
+const greet = function ({ name: companyName }) {
+  return `welcome to ${companyName}`;
+};
+console.log(greet(company));
+
+const jobs = ["SE", "PM", "DS", "QA"];
+for (const job of jobs) {
+  console.log(job);
+}
+
+for (const [index, job] of jobs.entries()) {
+  console.log(index);
+  console.log(job);
+}
+
+const jobs2 = new Set(["DS", "PM", "SE", "PM", "DS", "QA", "SE"]);
+console.log(jobs2);
+console.log(jobs2.size);
+console.log(jobs2.delete("QA"));
+console.log(jobs2.size);
+
+const jobs = new Map();
+jobs.set("Alex", "PM");
+jobs.set("Alice", "Dev");
+jobs.set(1, "test key type");
+console.log(jobs);
 */
+
+class Student {
+  #studentNumber;
+  #studentName;
+  constructor(studentNumber, studentName) {
+    this.#studentNumber = studentNumber;
+    this.#studentName = studentName;
+  }
+
+  set studentName(st) {
+    this.#studentName = st;
+  }
+  get studentName() {
+    return this.#studentName;
+  }
+  enroll(courseName) {
+    return `${this.#studentName} enrolled in ${courseName}`;
+  }
+}
+
+const s1 = new Student(1234, "Alex Nezami");
+console.log(s1);
+console.log(s1.studentName);
+console.log(s1.enroll("Internet security"));
